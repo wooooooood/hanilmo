54d1452f92262faec2581a1d25c91d6fb9a4c994
wooooooood <13pft13@gmail.com>
2021-05-16 02:20:30 +0900

    feat. add header

--- hanilmo/src/App.js
+++ hanilmo/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Router } from "./router/Router";
 import './App.css';
+import Header from "./component/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Router />
     </div>
   );
---
+++ hanilmo/src/component/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import title from "../img/title.png";
+
+function Header () {
+  return (
+    <>
+    <img src={title} />
+    </>
+  );
+}
+
+export default Header;

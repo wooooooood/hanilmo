75822934d5fe686fc810c07d5a724d9924dcb799
wooooooood <13pft13@gmail.com>
2021-05-17 00:11:28 +0900

    feat. add header style

--- hanilmo/src/component/Header.jsx
+++ hanilmo/src/component/Header.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import title from "../img/title.png";
+import styled from "styled-components";
+
+const LogoImg = styled.img`
+  &@media (max-width: 480px){
+    width: 90% !important;
+  }
+  width: 100%;
+  max-width: 400px;
+`;
 
 function Header () {
   return (
     <>
-    <img src={title} />
+    <LogoImg src={title} />
     </>
   );
 }

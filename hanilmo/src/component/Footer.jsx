dd7469dc836c6c7153d9b3240d741051182788da
wooooooood <13pft13@gmail.com>
2021-05-19 15:18:23 +0900

    feat. add footer component

---
+++ hanilmo/src/component/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+
+function Footer () {
+  return (
+    <>
+    <Container>
+    <FooterText>정품가발 한일모 | 대표 : 이상무 | 전화 : <a href="tel:055-292-4567" style={{textDecoration: 'none', color: '#000000'}}>055-292-4567</a></FooterText>
+<FooterText>경남 창원시 마산회원구 석전동 223-16(2층)</FooterText>
+<FooterText><strong>Copyright 명품가발 한일모 all rights reserved.</strong></FooterText>
+    </Container>
+    </>
+  );
+}
+
+export default Footer;
+
+const Container = styled.nav`
+  padding: 8px 0;
+border-top: 1px solid #E4E4E4;
+background-color: #F8F8F8;
+margin: auto;
+text-align: center;
+`;
+
+const FooterText = styled.div`
+font-size: 12px;
+font-weight: 400;
+padding: 2px 0;`;

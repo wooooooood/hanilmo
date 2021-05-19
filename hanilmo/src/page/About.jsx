c367634f1fd2496f5973a59cec6c9d3569687eac
wooooooood <13pft13@gmail.com>
2021-05-19 23:50:16 +0900

    feat. add About page

--- hanilmo/src/page/About.jsx
+++ hanilmo/src/page/About.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import styled from "styled-components";
+import about from "../img/about.jpg";
 
 export const About = () => {
   return (
@@ -7,7 +9,22 @@ export const About = () => {
       <Helmet>
         <title>한일모 :: 소개</title>
       </Helmet>
-      <h1>어바웃</h1>
+      <ImageContainer>
+        <Image src={about}/>
+      </ImageContainer>
     </>
   );
 };
+
+const ImageContainer = styled.div`
+  @media (max-width: 768px){
+    width: 100vw;
+  }
+  text-align: center;
+`;
+
+const Image = styled.img`
+  @media (max-width: 768px){
+    width: 100%;
+  }
+`;

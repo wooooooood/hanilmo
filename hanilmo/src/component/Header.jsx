4280b726add88041d3729355a51581f68a9c34b9
wooooooood <13pft13@gmail.com>
2021-05-19 15:22:44 +0900

    refactor. header

--- hanilmo/src/component/Header.jsx
+++ hanilmo/src/component/Header.jsx
@@ -2,22 +2,28 @@ import React from "react";
 import title from "../img/title.png";
 import styled from "styled-components";
 
-const LogoImg = styled.img`
-  &@media (max-width: 480px){
-    width: 90% !important;
-  }
-  width: 100%;
-  max-width: 400px;
-  margin: 10px auto;
-  display: block;
-`;
-
 function Header () {
   return (
-    <>
+    <Container>
     <LogoImg src={title} />
-    </>
+    </Container>
   );
 }
 
 export default Header;
+
+const Container=styled.div`
+background-color:#F8F8F8;
+padding: 0;
+margin: 0;
+`;
+
+const LogoImg = styled.img`
+  @media (max-width: 480px){
+    width: 90% !important;
+  }
+  width: 100%;
+  max-width: 400px;
+  margin: auto;
+  display: block;
+`;

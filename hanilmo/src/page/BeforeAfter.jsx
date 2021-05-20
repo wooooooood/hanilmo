48e9af876b884377b6589db41bd043b6826ca448
wooooooood <13pft13@gmail.com>
2021-05-20 23:21:31 +0900

    feat. add before after img

--- hanilmo/src/page/BeforeAfter.jsx
+++ hanilmo/src/page/BeforeAfter.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import styled from "styled-components";
+import before from "../img/before.jpg";
+import after from "../img/after.jpg";
+import front from "../img/front.jpg";
 
 export const BeforeAfter = () => {
   return (
@@ -7,7 +11,18 @@ export const BeforeAfter = () => {
       <Helmet>
         <title>한일모 :: 착용사진</title>
       </Helmet>
-      <h1>전후</h1>
+      <ImageContainer innerText={"착용 전"}><img src={before}/></ImageContainer>
+      <ImageContainer innerText={"착용 후"}><img src={after}/></ImageContainer>
+      <ImageContainer innerText={"정면"}><img src={front}/></ImageContainer>
     </>
   );
 };
+
+const ImageContainer = styled.div`
+  @media (max-width: 480px){
+    width: 90% !important;
+  }
+  width: 33.33%;
+  margin: auto;
+  display: inline-flex;
+`;//${({ width }) => width}px;

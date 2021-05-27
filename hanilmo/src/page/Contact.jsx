460c83bce5a7b993aaee2c02fec3e67cfe7d8ff8
wooooooood <13pft13@gmail.com>
2021-05-27 23:27:22 +0900

    feat. add contact address img

--- hanilmo/src/page/Contact.jsx
+++ hanilmo/src/page/Contact.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import styled from "styled-components";
+import addr from "../img/addr.jpg";
 
 export const Contact = () => {
   return (
@@ -7,7 +9,13 @@ export const Contact = () => {
       <Helmet>
         <title>한일모 :: 오시는길</title>
       </Helmet>
-      <h1>컨택</h1>
+      <AddressImg src={addr}/>
     </>
   );
 };
+
+const AddressImg = styled.img`
+@media (max-width: 768px)
+{
+    width: 100%;
+}`;

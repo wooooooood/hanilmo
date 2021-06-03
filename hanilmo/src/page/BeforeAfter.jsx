bbc194ec152518873328554fe5bd555158972f89
wooooooood <13pft13@gmail.com>
2021-06-03 23:55:56 +0900

    fix. before after page alignment

--- hanilmo/src/page/BeforeAfter.jsx
+++ hanilmo/src/page/BeforeAfter.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { Helmet } from "react-helmet";
-import styled from "styled-components";
-import before from "../img/before.jpg";
-import after from "../img/after.jpg";
-import front from "../img/front.jpg";
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import styled from 'styled-components';
+import before from '../img/before.jpg';
+import after from '../img/after.jpg';
+import front from '../img/front.jpg';
 
 export const BeforeAfter = () => {
   return (
@@ -11,18 +11,29 @@ export const BeforeAfter = () => {
       <Helmet>
         <title>한일모 :: 착용사진</title>
       </Helmet>
-      <ImageContainer innerText={"착용 전"}><img src={before}/></ImageContainer>
-      <ImageContainer innerText={"착용 후"}><img src={after}/></ImageContainer>
-      <ImageContainer innerText={"정면"}><img src={front}/></ImageContainer>
+      <Wrapper>
+        <ImageContainer innerText={'착용 전'}>
+          <img src={before} />
+        </ImageContainer>
+        <ImageContainer innerText={'착용 후'}>
+          <img src={after} />
+        </ImageContainer>
+        <ImageContainer innerText={'정면'}>
+          <img src={front} />
+        </ImageContainer>
+      </Wrapper>
     </>
   );
 };
 
+const Wrapper = styled.div`
+  text-align: center;
+`;
+
 const ImageContainer = styled.div`
-  @media (max-width: 480px){
+  @media (max-width: 480px) {
     width: 90% !important;
   }
-  width: 33.33%;
-  margin: auto;
+  margin: 15px;
   display: inline-flex;
-`;//${({ width }) => width}px;
+`; //${({ width }) => width}px;
